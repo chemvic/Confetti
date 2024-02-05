1deb68527101af1c84a481ab870da23a3b5e8e68
chemvic <chemvic79@gmail.com>
2024-02-05 11:06:15 +0200

    background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from 'components/Header/Header';
 import css from './App.module.css';
 import Hero from 'components/Hero/Hero';
+import Background from 'components/Background/Background';
 
 function App() {
   return (
     <div className={css.container}>
+      <Background/>
       <Header />
       <Hero/>
     </div>
---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,9 @@
+import css from './Background.module.css';
+
+const Background = () => {
+  return (
+    <div className={css.background}></div>
+  )
+}
+
+export default Background;
